f1fda5ca03c6a5907a36fc85d7607b17cb62a07d
Timothy Haas <tihaas@Timothys-MacBook-Pro.local>
2013-10-09 07:52:30 -0400

    FIX SPACING

--- Hypermedia_Request.php
+++ Hypermedia_Request.php
@@ -1,72 +1,72 @@
 <?php
 
 
-	class Hypermedia_Request {
-		
-		private $PROTOCOL = "http";
-		private $PROTOCOL_VER = "1.1";
+  class Hypermedia_Request {
 
-		private $uri = "";
-		private $verb	= "";
-		private $request_headers = ["Connection: Close","Host: localhost:4567"];
-		private $request_body = NULL;
+    private $PROTOCOL = "http";
+    private $PROTOCOL_VER = "1.1";
 
-		private $options = [];
-		private $context = "";
-		private $ignore_errors = true;
+    private $uri = "";
+    private $verb	= "";
+    private $request_headers = ["Connection: Close","Host: localhost:4567"];
+    private $request_body = NULL;
 
-		private $response = "";
-		private $response_headers = [];
+    private $options = [];
+    private $context = "";
+    private $ignore_errors = true;
 
-		public function __construct($verb, $uri, $headers=array(), $request_body=NULL){
-			$this->verb = $verb;	// Set the verb 
-			$this->uri 	= $uri;		// Set the URI
+    private $response = "";
+    private $response_headers = [];
 
-			if (!empty($headers)) {
-				foreach ($headers as $header) { array_push($this->request_headers, $header); }	// Set the headers, if they exist
-			}
-			if (!is_null($request_body)) { $this->request_body = $request_body; }	// Set the request body, if it exists
-		}
+public function __construct($verb, $uri, $headers=array(), $request_body=NULL){
+$this->verb = $verb;	// Set the verb 
+$this->uri 	= $uri;		// Set the URI
 
-		public function execute(){
-			// Build out the options hash
-			$this->options[$this->PROTOCOL]['protocol_version'] = $this->PROTOCOL_VER;	// Set the HTTP version
-			$this->options[$this->PROTOCOL]['method'] 	= $this->verb;					// Set the verb
-			$this->options[$this->PROTOCOL]['header'] 	= $this->request_headers;		// Add the headers
-			$this->options[$this->PROTOCOL]['content']	= $this->request_body;			// Add the content
-			$this->options[$this->PROTOCOL]['ignore_errors'] = $this->ignore_errors;	// Read the response body on errors (per rfc-2616)
+if (!empty($headers)) {
+foreach ($headers as $header) { array_push($this->request_headers, $header); }	// Set the headers, if they exist
+}
+if (!is_null($request_body)) { $this->request_body = $request_body; }	// Set the request body, if it exists
+}
 
-			$this->context = stream_context_create($this->options);						// Build the HTTP context stream (whatever the hell that is)
+public function execute(){
+// Build out the options hash
+$this->options[$this->PROTOCOL]['protocol_version'] = $this->PROTOCOL_VER;	// Set the HTTP version
+$this->options[$this->PROTOCOL]['method'] 	= $this->verb;					// Set the verb
+$this->options[$this->PROTOCOL]['header'] 	= $this->request_headers;		// Add the headers
+$this->options[$this->PROTOCOL]['content']	= $this->request_body;			// Add the content
+$this->options[$this->PROTOCOL]['ignore_errors'] = $this->ignore_errors;	// Read the response body on errors (per rfc-2616)
 
-			// FIRE!! 
-			$this->response = file_get_contents($this->uri, false, $this->context);
+$this->context = stream_context_create($this->options);						// Build the HTTP context stream (whatever the hell that is)
 
-			// Read the response headers and add them into the object
-			// The $http_response_header array is similar to the get_headers() function. 
-			foreach ($http_response_header as $header){
-				// The status code is special: it doesn't have a colon
-				if (preg_match("/http/i", $header)) {
-					$this->response_headers['Status'] = $header;
-				} 
-				// Everything else
-				else {
-					$pieces = explode(":", $header, 2);
-					$this->response_headers[$pieces[0]] = $pieces[1];
-				}
-			}
-		}
+// FIRE!! 
+$this->response = file_get_contents($this->uri, false, $this->context);
 
-		public function getResponseBody(){
-			return $this->response;
-		}
+// Read the response headers and add them into the object
+// The $http_response_header array is similar to the get_headers() function. 
+foreach ($http_response_header as $header){
+// The status code is special: it doesn't have a colon
+if (preg_match("/http/i", $header)) {
+$this->response_headers['Status'] = $header;
+} 
+// Everything else
+else {
+$pieces = explode(":", $header, 2);
+$this->response_headers[$pieces[0]] = $pieces[1];
+}
+}
+}
 
-		public function getResponseHeaders(){
-			return $this->response_headers;
-		}
+public function getResponseBody(){
+return $this->response;
+}
 
-		public function getOptions(){
-			return $this->options;
-		}
+public function getResponseHeaders(){
+return $this->response_headers;
+}
+
+public function getOptions(){
+return $this->options;
+}
 
 }
 
